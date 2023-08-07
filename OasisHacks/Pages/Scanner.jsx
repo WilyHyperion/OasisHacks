7519abcfd6f4063c6135157f7982e36c0d32d5a8
Gabibag <97263038+Gabibag@users.noreply.github.com>
2023-08-06 17:47:08 -0700

    added splash and stuff

--- OasisHacks/Pages/Scanner.jsx
+++ OasisHacks/Pages/Scanner.jsx
@@ -1,18 +1,27 @@
 import * as React from 'react';
-import { Text, StyleSheet, Touchable, TouchableOpacity} from 'react-native';
+import {Text, StyleSheet, Touchable, TouchableOpacity, View} from 'react-native';
 import { Camera, CameraType } from 'expo-camera';
 import {BarCodeScanner} from 'expo-barcode-scanner'
 
 const styles = StyleSheet.create({
     cam: {
-        width: '100%',
-        height: '80%',
+        width: '95%',
+        height: '93%',
         position: 'absolute',
-        left: 0,
-        top: '10%',
+        left: '2.5%',
+        top: '5%',
+        borderRadius: 20,
         flex: 1,
 
     },
+    container: {
+        flex: 1,
+        backgroundColor: '#fefae0',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%',
+        height: '100%',
+      },
 });
 function press(){
     console.log("pressed");
@@ -35,6 +44,8 @@ export default  function Home(props) {
 
     const [permission, requestPermission] = Camera.useCameraPermissions();
     return (<>
+        {/*<View style={styles.container}>*/}
     {permission?.granted ? (<BarCodeScanner style = {styles.cam} onBarCodeScanned={ handleBarCodeScanned}></BarCodeScanner>) : <Text>Permission not granted</Text>}
+        {/*</View>*/}
     </>);
 }
